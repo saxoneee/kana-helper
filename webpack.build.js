b98841ef54af30317d613ed77963d0ef284e3e9b
Hendrik Sachse <hendrik.sachse@gmail.com>
2021-02-11 20:53:50 +0100

    move webpack dev output to another folder

---
+++ webpack.build.js
@@ -0,0 +1,47 @@
+var path = require('path'),
+	{ CleanWebpackPlugin } = require('clean-webpack-plugin'),
+	CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: './src/js/main.js',
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	},
+
+	devServer: {
+		open: false,
+		contentBase: [
+			path.join(__dirname, 'src'),
+			path.join(__dirname, 'node_modules'),
+		],
+		port: 8000,
+		// host: '0.0.0.0' // use this with your local ip-address for debugging on mobile
+	},
+
+	plugins: [
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [{
+				from: './src/index.html',
+				to: './'
+			},{
+				from: './src/js/main.js',
+				to: './js'
+			},{
+				from: './src/css/styles.css',
+				to: './css'
+			},{
+				from: './node_modules/jquery/dist/jquery.min.js',
+				to: './jquery/dist'
+			},{
+				from: './src/data/kana.json',
+				to: './data'
+			},{
+				from: './src/data/keybinding.json',
+				to: './data'
+			}
+		]
+		})
+	]
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,47 +1,10 @@
+// enrich webpack cfg for development
+
 var path = require('path'),
-	{ CleanWebpackPlugin } = require('clean-webpack-plugin'),
-	CopyWebpackPlugin = require('copy-webpack-plugin');
+    webpackBuildCfg = require('./webpack.build.js');
 
-module.exports = {
-	entry: './src/js/main.js',
-	mode: 'development',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-	},
-
-	devServer: {
-		open: false,
-		contentBase: [
-			path.join(__dirname, 'src'),
-			path.join(__dirname, 'node_modules'),
-		],
-		port: 8000,
-		// host: '0.0.0.0' // use this with your local ip-address for debugging on mobile
-	},
-
-	plugins: [
-		new CleanWebpackPlugin(),
-		new CopyWebpackPlugin({
-			patterns: [{
-				from: './src/index.html',
-				to: './'
-			},{
-				from: './src/js/main.js',
-				to: './js'
-			},{
-				from: './src/css/styles.css',
-				to: './css'
-			},{
-				from: './node_modules/jquery/dist/jquery.min.js',
-				to: './jquery/dist'
-			},{
-				from: './src/data/kana.json',
-				to: './data'
-			},{
-				from: './src/data/keybinding.json',
-				to: './data'
-			}
-		]
-		})
-	]
+webpackBuildCfg.output = {
+    path: path.resolve(__dirname, 'tmp'), // TODO, not existing folder, but may cause trouble some time
 };
+
+module.exports = webpackBuildCfg;
